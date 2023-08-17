33a771fb13b25b473e3068b45d7dfd3e953fea9c
David Álvarez <david.alvarez.celemin@udc.es>
2023-08-17 02:06:25 +0200

    Added runSaveState

---
+++ Assets/Scripts/RunSaveState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "runSS", menuName = "new run_saveState")]
+public class RunSaveState : ScriptableObject  //Contains information relevant to the current run/play of the game
+{
+    public int roomNo = 0;
+    public Sprite character; //If we want to add buffs to certain characters we need to change this
+    public Card[] Deck;
+    public Card[] Collection;
+}
